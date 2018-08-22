d9fda240baf55a0c58d0539639ee2a89d9bca759
Jliv316 <jclivingston316@gmail.com>
2018-08-22 14:43:25 -0600

    getMeals model method

--- models/meal.js
+++ models/meal.js
@@ -5,10 +5,15 @@ const database = require('knex')(configuration);
 class Meal {
   static async getMeals(req, res) {
     let meals = await database('meals').select();
-    let meal_foods = await meals.map((meal) => {
-      
-    })
+    let allMeals = await meals.map((meal) => {
+      let foods = await database('foods').select('foods.id', 'foods.name', 'foods.calories')
+        .innerJoin('meal_foods', 'foods.id', 'meal_foods.food_id')
+        .where('meal_foods.meal_id', meal.id)
+      meal['foods'] = foods;
 
+      return meal
+    })
+    console.log(allMeals);
   }
 
   static async getMeal(req, res) {
