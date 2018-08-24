29caeb61133ea06497aea4a9a311a023fb933ce4
Jliv316 <jclivingston316@gmail.com>
2018-08-24 17:30:29 -0600

    test

--- models/meal.js
+++ models/meal.js
@@ -36,8 +36,6 @@ class Meal {
   // static async getMeal(req, res) {
   //   let meal_id = req.params.meal_id
   //   let meal = await database('meals').where('id', meal_id).select();
-  //   let foods = await database('foods').select('foods.id', 'foods.name', 'foods.calories')
-  //     .innerJoin('meal_foods', 'foods.id', 'meal_foods.food_id')
 
   static async createMealFood(req, res) {
     let meal_id = req.params.meal_id;
