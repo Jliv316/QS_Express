7dea43b3942935f8cb8802a2b51cfd47dbbb8f12
Jliv316 <jclivingston316@gmail.com>
2018-09-04 15:04:33 -0600

    fetches yummly api

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,10 +1,12 @@
 const Recipe = require('../models/recipe')
+const Food = require('../models/food')
 
 class RecipesController {
 
   static async show(req, res) {
-    let food = await Recipe.getFood(req, res);
-    res.status(200).json(food);
+    let food = await Food.getFood(req, res);
+    let recipes = await Recipe.yummlyCall(food);
+    res.status(200).json(recipes);
   }
 }
 
--- models/recipe.js
+++ models/recipe.js
@@ -5,25 +5,20 @@ var express = require('express');
 var request = require('request');
 var app = express(); 
 
+require("isomorphic-fetch")
+
 class Recipe {
 
-  static async getFood(req, res) {
-    let result = await database('foods').where('id', req.params.id).select();
-    let food = result[0].name;
-    request(`http://api.yummly.com/v1/api/recipes?_app_id=58de4c61&_app_key=49c29c78d808d440d16df6b0182eeab3&q=${food}`, { json: true }, (err, res, body) => {
-      if (err) { return console.log(err); }
-      
-      let recipes = {};
-      let matches = res.body.matches;
-     recipes["recipes"] = matches.map((match) => {
-        let recipe = {name: match.recipeName, url: match.imageUrlsBySize}
-        return recipes
-      })
-      console.log(recipes[0].name)
-    });
+  static async yummlyCall(food){
+    let response = await fetch(`http://api.yummly.com/v1/api/recipes?_app_id=58de4c61&_app_key=49c29c78d808d440d16df6b0182eeab3&q=${food}`);
+    let data = await response.json();
+    let matches = await data.matches.map((match) => {
+      return { name: match.recipeName, url: `http://www.yummly.com/recipe/${match.id}`};
+    })
+    let recipes = {};
+    recipes["recipes"] = matches;
     return recipes;
-  }
+  } 
 }
-
 
 module.exports = Recipe;
